e20f98b17e4eabbd262760fa5ecdc0c07e603785
g119446316 <40300573+g119446316@users.noreply.github.com>
2020-12-20 18:09:34 +0800

    Update nvidia_data.py

--- nvidia_data.py
+++ nvidia_data.py
@@ -5,7 +5,15 @@ from jtop import jtop
 import json
 import datetime
 import os
+import threading
 import re
+
+port = '/dev/ttyACM0'
+ard = serial.Serial(port,115200,timeout=3,
+           parity=serial.PARITY_NONE,
+           bytesize=serial.EIGHTBITS,
+           stopbits=serial.STOPBITS_ONE)
+time.sleep(2)
 
 def myconverter(o):
     if isinstance(o, datetime.datetime):
@@ -35,7 +43,7 @@ def changedict(data):
 
 def send_json():
   while True:
-   # if ard.isOpen():
+    if ard.isOpen():
       data=openjtop() #get jtop stats
       closejtop()
       data=changedict(data) #change dict
@@ -46,14 +54,14 @@ def send_json():
       d1="["+d1+"]"
       d2="["+d2+"]"
 
-      print(d1,d2)
-
-send_json()
-"""
       ard.write(d1.encode('ascii'))
       ard.flush()
       time.sleep(2)
       ard.write(d2.encode('ascii'))
       ard.flush()
       time.sleep(5)
-"""
+
+
+if __name__ == '__main__' :
+     send_json()
+
