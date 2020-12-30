15918aeb69c8f9c144c877b7fcd8eeecb9ef2950
g119446316 <40300573+g119446316@users.noreply.github.com>
2020-12-30 16:52:31 +0800

    Create readline_socketclient.py

---
+++ readline_socketclient.py
@@ -0,0 +1,45 @@
+
+import re
+import os
+import serial
+import time
+import socket
+
+port = '/dev/ttyUSB0' # note I'm using Mac OS-X
+ard = serial.Serial(port,115200,timeout=3,
+           parity=serial.PARITY_NONE,
+           bytesize=serial.EIGHTBITS,
+           stopbits=serial.STOPBITS_ONE)
+time.sleep(2)
+
+
+
+
+def readline():
+
+  
+  while True:
+    try:
+       incoming = ard.readline().decode('ascii')
+       print(incoming)
+       ard.flush()
+       if  "Received" in incoming :
+         HOST = socket.gethostbyname('ef8349423a46')
+         PORT = 8000
+         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+         client.connect((HOST, PORT))
+         s=re.findall("\d+",incoming)[0]
+         client.send(s.encode())
+         client.close()
+         """
+         s=re.findall("\d+",incoming)[0]
+         print(s)
+         oscmd_1="sh -c 'echo "
+         oscmd_2="> /sys/devices/pwm-fan/target_pwm'"
+         os.system(oscmd_1+str(s)+oscmd_2)
+         """
+    except:
+       pass
+
+if __name__ == '__main__' :
+     readline()
