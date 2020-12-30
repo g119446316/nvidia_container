cc2994bb4df262b2f8c07a0bb674ef31aac72bce
g119446316 <40300573+g119446316@users.noreply.github.com>
2020-12-30 16:59:38 +0800

    Create control_fan_socketserver.py

---
+++ control_fan_socketserver.py
@@ -0,0 +1,21 @@
+import socket
+import os
+
+HOST = ''
+PORT = 8000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen(1)
+
+while True:
+    conn, addr = server.accept()
+    clientMessage = str(conn.recv(1024).encode('utf-8'))
+
+    print('Client message is:', clientMessage)
+    
+    oscmd_1="sh -c 'echo "
+    oscmd_2="> /sys/devices/pwm-fan/target_pwm'"
+    os.system(oscmd_1+str(clientMessage)+oscmd_2)
+
+    conn.close()
