bad2ccd4f6f9cc87a608812396553dbeb0ec6fcb
g119446316 <40300573+g119446316@users.noreply.github.com>
2021-01-17 15:34:53 +0800

    Update psutil_popen.py

--- psutil_popen.py
+++ psutil_popen.py
@@ -1,30 +1,57 @@
 
 import psutil
 import time
-from popen import Sh
+import subprocess 
+import os
+
+total_info = {}
 
 while True:
   #cpu
-  cpu_count = psutil.cpu_count()
-  cpu_percent = psutil.cpu_percent(interval=1,percpu=True)
+  cpu_usage = psutil.cpu_percent(interval=1,percpu=True)
   cpu_freq = psutil.cpu_freq()
 
-  #getloadavg
+  for i in range(len(cpu_usage)):
+    usage = { "cpu"+str(i+1) : cpu_usage[i] }
+    total_info.update(usage)
+
+  total_info.update({"cpu_freq" : cpu_freq[0]})
+
+  #getloadavg 1,5,15m
   loadavg = psutil.getloadavg()
+  total_info.update({"loadavg" : loadavg[2]})
 
   #memory
-  virtual_memory = psutil.virtual_memory()
+  mem = psutil.virtual_memory()
+  total_info.update({"mem" : mem[2]})
+
+  #gpu_usage
+  gpu = subprocess.Popen(['cat', '/sys/devices/gpu.0/load'],stdout=subprocess.PIPE)
+  gpu.wait()
+  total_info.update({"gpu" : float(gpu.stdout.read())/10 })
 
   #fan_speed
-  fan = Sh('cat', '/sys/devices/pwm-fan/target_pwm')
+  fan = subprocess.Popen(['cat', '/sys/devices/pwm-fan/target_pwm'],stdout=subprocess.PIPE)
+  fan.wait()
+  total_info.update({"fan" : int(fan.stdout.read()) })
 
   #AO-therm,CPU-therm,GPU-therm,PLL-therm,PMIC-Die,thermal-fan-est
-  thermal_type = Sh('cat', '/sys/devices/virtual/thermal/thermal_zone*/type')
-  thermal_temp = Sh('cat', '/sys/devices/virtual/thermal/thermal_zone*/temp')
+  thermal_type = os.popen('cat /sys/devices/virtual/thermal/thermal_zone*/type')
+  thermal_temp = os.popen('cat /sys/devices/virtual/thermal/thermal_zone*/temp')
+  thermal_type_name = thermal_type.readlines()
+  thermal_type_temp = thermal_temp.readlines()
+
+  for i in range(len(thermal_type_name)):
+     thermal_list = { str(thermal_type_name[i]).replace("\n","") : int(thermal_type_temp[i])/1000 }
+     total_info.update(thermal_list)
+
+  del total_info['PMIC-Die']
 
   #disk
   disk =  psutil.disk_usage('/')
+  total_info.update({"disk" : disk[3] })
 
-  print(cpu_freq)
-  time.sleep(3)
+  print(total_info)
+
+  time.sleep(5)
 
